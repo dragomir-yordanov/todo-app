69107a0b2558994fac473b15505dfb284232b1c6
dragomir-yordanov <dragomirjordanov@gmail.com>
2025-09-17 16:23:24 +0300

    Update main.py
    added more functionality

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello, world!") 
+print("Hello, World!")
+print("This is my todo app!")
+
+todos = []
+
+def add_todo(task):
+    todos.append(task)
+    print(f"Added: {task}")
+
+# Test it
+add_todo("Learn Git and GitHub")
+print(f"Current todos: {todos}")
